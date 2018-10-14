7171b76b1d089e8bd3e0be11efdb9f244534d5f4
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-10-14 12:24:05 +0200

    feat(validations): add validations to fighters

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,2 +1,18 @@
 class Fighter < ApplicationRecord
+
+  validates :name, :health, :strengh, :experience, :level, presence: true
+
+  before_validation :set_default_fighter, on: :create
+
+
+private
+
+  def set_default_fighter
+    self.health     = 10
+    self.strengh    = 10
+    self.experience = 0
+    self.level      = 1
+    self.available_upgrade = 10
+  end
+
 end
