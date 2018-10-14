f03b52060405ceae244d8f30441113b6cbca3d82
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-10-14 10:59:21 +0200

    feat(fighter): add fighters

---
+++ db/migrate/20181014085420_create_fighters.rb
@@ -0,0 +1,14 @@
+class CreateFighters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :fighters do |t|
+      t.string :name
+      t.integer :health
+      t.integer :strengh
+      t.integer :experience
+      t.integer :level
+      t.integer :available_upgrade
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/fighters.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :fighter do
+    name { "MyString" }
+    health { 1 }
+    strengh { 1 }
+    experience { 1 }
+    level { 1 }
+    available_upgrade { 1 }
+  end
+end
