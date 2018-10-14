b09388545930ecd8b1d3cd0064b674546c17ff99
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-10-14 19:45:17 +0200

    test(factory): add additionnal trait for fighter without available_upgrade

--- spec/factories/fighters.rb
+++ spec/factories/fighters.rb
@@ -7,10 +7,17 @@ FactoryBot.define do
       to_create {|instance| instance.save(validate: false) }
 
       health              { Faker::Number.between(1,100) }
-      strength             { Faker::Number.between(1,100) }
+      strength            { Faker::Number.between(1,100) }
       experience          { Faker::Number.between(1,100) }
       level               { Faker::Number.between(1,100) }
       available_upgrade   { Faker::Number.between(1,100) }
+    end
+
+    trait :without_avaible_upgrade do
+      # Turn off validations so we can create fighter without experience
+      to_create {|instance| instance.save(validate: false) }
+
+      available_upgrade   0
     end
   end
 end
