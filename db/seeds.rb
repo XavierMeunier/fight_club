e3448348afd2f1edc803618c0af89af389417a7b
Xavier Meunier <m.xaviermeunier@gmail.com>
2018-10-14 12:35:44 +0200

    feat(seed): add default fighters in app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create 5 beginners
+5.times do 
+  Fighter.create({name: Faker::Name.name})
+end
+
+# Create 30 advanced fighters
+30.times do 
+  level   = Faker::Number.between(1,100)
+  available_upgrade = level*10
+  health  = Faker::Number.between(1,available_upgrade)
+  strengh = available_upgrade - health
+
+  fighter = Fighter.new({
+    name:         Faker::Name.name,
+    health:       health,
+    strengh:      strengh,
+    experience:   0,
+    level:        level,
+    available_upgrade: 0
+  })
+  fighter.save(validate: false)
+end
